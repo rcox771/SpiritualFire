d47139d7ae344861742e737129b45aefd39cdfde
Xellophane <rinotfox@gmail.com>
2020-10-07 16:34:37 -0600

    couchdb + initial app
    Couchdb has been set, along with the initial app for django. No DB work
    or documents have been made.

--- SpiritFire/SpiritFire/settings.py
+++ SpiritFire/SpiritFire/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'spiritfire.landing',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
